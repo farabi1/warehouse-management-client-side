0db18c237cef3505768ca84e58b4a7463ce6e423
farabi1 <rashidfarabi@gmail.com>
2022-05-03 22:01:49 +0600

    React Router 6  Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Eleven Assignment</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
