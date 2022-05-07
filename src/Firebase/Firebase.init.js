4c31efbcd9ab0ec0a322755b12afd3f321540ae9
farabi1 <rashidfarabi@gmail.com>
2022-05-07 19:43:48 +0600

    Firebase Setup

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDE6bDJbEBRLF3ExY_DbH49QwoN66oiN2U",
+    authDomain: "warehouse-client-side-85d59.firebaseapp.com",
+    projectId: "warehouse-client-side-85d59",
+    storageBucket: "warehouse-client-side-85d59.appspot.com",
+    messagingSenderId: "645493424",
+    appId: "1:645493424:web:d5c370828f23a079834175"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
