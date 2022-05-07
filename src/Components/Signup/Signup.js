d92e30d4bc6a008a6c47ac6df4c4f3c98081e0e1
farabi1 <rashidfarabi@gmail.com>
2022-05-08 01:56:16 +0600

    Firebase hook

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -34,69 +34,69 @@ function Signup() {
             });
     };
 
-    const handleEmail = (event) => {
-        const emailInput = event.target.value;
-        if (/\S+@\S+\.\S+/.test(emailInput)) {
-            setEmail({ value: emailInput, error: "" });
-        } else {
-            setEmail({ value: "", error: "Please Provide a valid Email" });
-        }
-    };
-    const handlePassword = (event) => {
-        const passwordInput = event.target.value;
+    // const handleEmail = (event) => {
+    //     const emailInput = event.target.value;
+    //     if (/\S+@\S+\.\S+/.test(emailInput)) {
+    //         setEmail({ value: emailInput, error: "" });
+    //     } else {
+    //         setEmail({ value: "", error: "Please Provide a valid Email" });
+    //     }
+    // };
+    // const handlePassword = (event) => {
+    //     const passwordInput = event.target.value;
 
-        if (passwordInput.length < 7) {
-            setPassword({ value: "", error: "Password too short" });
-        } else if (!/(?=.*[A-Z])/.test(passwordInput)) {
-            setPassword({
-                value: "",
-                error: "Password must contain a capital letter",
-            });
-        } else {
-            setPassword({ value: passwordInput, error: "" });
-        }
-    };
-    const handleConfirmPassword = (event) => {
-        const confirmationInput = event.target.value;
+    //     if (passwordInput.length < 7) {
+    //         setPassword({ value: "", error: "Password too short" });
+    //     } else if (!/(?=.*[A-Z])/.test(passwordInput)) {
+    //         setPassword({
+    //             value: "",
+    //             error: "Password must contain a capital letter",
+    //         });
+    //     } else {
+    //         setPassword({ value: passwordInput, error: "" });
+    //     }
+    // };
+    // const handleConfirmPassword = (event) => {
+    //     const confirmationInput = event.target.value;
 
-        if (confirmationInput !== password.value) {
-            setPasswordConfirmation({ value: "", error: "Password Mismatched" });
-        } else {
-            setPasswordConfirmation({ value: confirmationInput, error: "" });
-        }
-    };
+    //     if (confirmationInput !== password.value) {
+    //         setPasswordConfirmation({ value: "", error: "Password Mismatched" });
+    //     } else {
+    //         setPasswordConfirmation({ value: confirmationInput, error: "" });
+    //     }
+    // };
 
-    const handleSignup = (event) => {
-        event.preventDefault();
-        if (email.value === "") {
-            setEmail({ value: "", error: "Email is required" });
-        }
-        if (password.value === "") {
-            setPassword({ value: "", error: "Password is required" });
-        }
-        if (passwordConfirmation.value === "") {
-            setPasswordConfirmation({
-                value: "",
-                error: "Password confirmation is required",
-            });
-        }
-        if (email.value && password.value === passwordConfirmation.value) {
-            createUserWithEmailAndPassword(auth, email.value, password.value)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    toast.success("Account created", { id: "created" });
-                    navigate("/");
-                })
-                .catch((error) => {
-                    const errorMessage = error.message;
-                    if (errorMessage.includes("already-in-use")) {
-                        toast.error("Email already in use", { id: "error" });
-                    } else {
-                        toast.error(errorMessage, { id: "error" });
-                    }
-                });
-        }
-    };
+    // const handleSignup = (event) => {
+    //     event.preventDefault();
+    //     if (email.value === "") {
+    //         setEmail({ value: "", error: "Email is required" });
+    //     }
+    //     if (password.value === "") {
+    //         setPassword({ value: "", error: "Password is required" });
+    //     }
+    //     if (passwordConfirmation.value === "") {
+    //         setPasswordConfirmation({
+    //             value: "",
+    //             error: "Password confirmation is required",
+    //         });
+    //     }
+    //     if (email.value && password.value === passwordConfirmation.value) {
+    //         createUserWithEmailAndPassword(auth, email.value, password.value)
+    //             .then((userCredential) => {
+    //                 const user = userCredential.user;
+    //                 toast.success("Account created", { id: "created" });
+    //                 navigate("/");
+    //             })
+    //             .catch((error) => {
+    //                 const errorMessage = error.message;
+    //                 if (errorMessage.includes("already-in-use")) {
+    //                     toast.error("Email already in use", { id: "error" });
+    //                 } else {
+    //                     toast.error(errorMessage, { id: "error" });
+    //                 }
+    //             });
+    //     }
+    // };
 
     return (
         <div>
@@ -181,13 +181,13 @@ function Signup() {
 
 
 
-                    <form onSubmit={handleSignup}>
+                    <form >
 
                         <div className='input-field'>
                             <label htmlFor='email'>Email</label>
                             <div className='input-wrapper'>
                                 <input
-                                    onBlur={handleEmail}
+                                    
                                     type='email'
                                     name='email'
                                     id='email'
